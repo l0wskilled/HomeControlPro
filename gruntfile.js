70195ae5196de16a3cf96ac260178f400f095953
Hendrik Knigge <h.knigge@level9.de>
2016-09-17 11:59:53 +0200

    change grunt from contao

--- gruntfile.js
+++ gruntfile.js
@@ -10,58 +10,50 @@ module.exports = function (grunt) {
             all: {
                 options: {
                     create: [
-                        'files/layout/resources/js',
-                        'files/layout/resources/build/js',
-                        'files/layout/resources/build/css',
-                        'files/layout/resources/build/img',
-                        'files/layout/resources/sass',
-                        'files/layout/resources/img'
+                        'src/js',
+                        'dist/js',
+                        'dist/css',
+                        'dist/img',
+                        'src/sass',
+                        'src/img'
                     ]
                 }
             }
         },
         'file-creator': {
             basic: {
-                'files/layout/resources/sass/global.scss': function (fs, fd, done) {
+                'src/sass/global.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '//Import your files here!\n@import "box-sizing";\n@import "vars";\n@import "mixins";\n@import "predefined";\n@import "header";\n@import "main";\n@import "footer";\n@import "left";\n@import "right";\n');
                     done();
                 },
-                'files/layout/resources/sass/_box-sizing.scss': function (fs, fd, done) {
+                'src/sass/_box-sizing.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '*,\n*:after,\n*:before {\n    box-sizing: border-box;\n}');
                     done();
                 },
-                'files/layout/resources/sass/_vars.scss': function (fs, fd, done) {
-                    fs.writeSync(fd, '//insert $vars here\n$pathImg: "/files/layout/resources/build/img/";\n$white: #ffffff;\n$black: #000000;\n');
+                'src/sass/_vars.scss': function (fs, fd, done) {
+                    fs.writeSync(fd, '//insert $vars here\n$pathImg: "/dist/img/";\n$white: #ffffff;\n$black: #000000;\n');
                     done();
                 },
-                'files/layout/resources/sass/_mixins.scss': function (fs, fd, done) {
+                'src/sass/_mixins.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '@import "vars";\n\n@mixin svgFallback($svg, $png) {\n    background-image: url("#{$pathImg}#{$png}");\n    background-image: url("#{$pathImg}#{$svg}"), none;\n}\n\n@function str-replace($search, $replace, $subject) {\n    $index: str-index($subject, $search);\n\n    @if $index {\n        @return str-slice($subject, 1, $index - 1) + $replace + str-replace(str-slice($subject, $index + str-length($search)), $search, $replace);\n    }\n\n    @return $subject;\n}\n\n@function pxToRem($px) {\n    //to rem is default\n    //grunt plugin handles rem fallback\n    @if (type_of($px) == string) {\n        $px: str-replace("px", "", $px);\n        $px: str-replace(" ", "", $px);\n    }\n    @return #{($px/16) + "rem"};\n}\n\n@function pxToEm($px) {\n    @if (type_of($px) == string) {\n        $px: str-replace("px", "", $px);\n        $px: str-replace(" ", "", $px);\n    }\n    @return #{($px/16) + "em"};\n}\n\n@function pxToPercent($px) {\n    @if (type_of($px) == string) {\n        $px: str-replace("px", "", $px);\n        $px: str-replace(" ", "", $px);\n    }\n    @return #{(($px/16) * 100) + "%"};\n}\n\n@mixin cf() {\n    &:before, &:after {\n        content: "";\n        display: table;\n    }\n    &:after {\n        clear: both;\n    }\n}\n');
                     done();
                 },
-                'files/layout/resources/sass/_predefined.scss': function (fs, fd, done) {
+                'src/sass/_predefined.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\ndiv.inside {\n    @include cf;\n}\n\n');
                     done();
                 },
-                'files/layout/resources/sass/_header.scss': function (fs, fd, done) {
+                'src/sass/_header.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\n#header {\n\n}\n');
                     done();
                 },
-                'files/layout/resources/sass/_main.scss': function (fs, fd, done) {
+                'src/sass/_main.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\n#main {\n\n}\n');
                     done();
                 },
-                'files/layout/resources/sass/_footer.scss': function (fs, fd, done) {
+                'src/sass/_footer.scss': function (fs, fd, done) {
                     fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\n#footer {\n\n}\n');
                     done();
                 },
-                'files/layout/resources/sass/_left.scss': function (fs, fd, done) {
-                    fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\n#left {\n\n}\n');
-                    done();
-                },
-                'files/layout/resources/sass/_right.scss': function (fs, fd, done) {
-                    fs.writeSync(fd, '@import "vars";\n@import "mixins";\n\n#right {\n\n}\n');
-                    done();
-                }
             }
         },
         concat: {
@@ -70,19 +62,19 @@ module.exports = function (grunt) {
                 sourceMap: true
             },
             dist: {
-                // src: ['files/layout/resources/js/**/*.js', 'vendor/jquery/jquery.min.js'] vendor scripts
-                src: ['files/layout/resources/js/**/*.js'],
-                dest: 'files/layout/resources/build/js/global.js'
+                // src: ['src/js/**/*.js', 'vendor/jquery/jquery.min.js'] vendor scripts
+                src: ['src/js/**/*.js'],
+                dest: 'dist/js/global.js'
             }
         },
         uglify: {
             options: {
                 sourceMap: true,
-                sourceMapIn: 'files/layout/resources/build/js/global.js.map'
+                sourceMapIn: 'dist/js/global.js.map'
             },
             build: {
-                src: 'files/layout/resources/build/js/global.js',
-                dest: 'files/layout/resources/build/js/global.min.js'
+                src: 'dist/js/global.js',
+                dest: 'dist/js/global.min.js'
             }
         },
         sass: {
@@ -93,9 +85,9 @@ module.exports = function (grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: 'files/layout/resources/sass/',
+                    cwd: 'src/sass/',
                     src: ['**/*.scss'],
-                    dest: 'files/layout/resources/build/css',
+                    dest: 'dist/css',
                     ext: '.css'
                 }]
             }
@@ -113,7 +105,7 @@ module.exports = function (grunt) {
                 ]
             },
             dist: {
-                src: 'files/layout/resources/build/css/global.css'
+                src: 'dist/css/global.css'
             }
         },
         imagemin: {
@@ -123,9 +115,9 @@ module.exports = function (grunt) {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'files/layout/resources/img/',
+                    cwd: 'src/img/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: 'files/layout/resources/build/img/'
+                    dest: 'dist/img/'
                 }]
             }
         },
@@ -133,23 +125,23 @@ module.exports = function (grunt) {
             main: {
                 files: [{
                     expand: true,
-                    cwd: 'files/layout/resources/img/',
+                    cwd: 'src/img/',
                     src: ['**/*.svg'],
-                    dest: 'files/layout/resources/build/img/'
+                    dest: 'dist/img/'
                 }]
             }
         },
         watch: {
             scripts: {
-                files: ['files/layout/resources/js/**/*.js'],
+                files: ['src/js/**/*.js'],
                 tasks: ['js']
             },
             sass: {
-                files: ['files/layout/resources/sass/**/*.scss'],
+                files: ['src/sass/**/*.scss'],
                 tasks: ['style']
             },
             img: {
-                files: [['files/layout/resources/img/**/*.{png,jpg,gif,svg}']],
+                files: [['src/img/**/*.{png,jpg,gif,svg}']],
                 tasks: ['img']
             }
         }
